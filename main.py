af63c8a6fd5e0134841aa3270524379a1f89871f
nguyenmkha557-a11y <nguyenmkha557@gmail.com>
2026-07-24 07:20:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from playwright.async_api import BrowserContext, Page, Route, async_playwright
 # =========================
 # CẤU HÌNH
 # =========================
-TARGET_URL = "https://live03.chuoichientv.me/"
+TARGET_URL = "https://live04.chuoichientv.me/"
 PLAYER_ORIGIN_FALLBACK = "https://live.chuoichien.tv"
 OUTPUT_M3U = "chuoichien_live.m3u"
 OUTPUT_PIPE_M3U = "chuoichien_live_pipe.m3u"
